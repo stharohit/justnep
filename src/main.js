9013dbdd181be97e62ad10489040626d9ef9d8b9
GwaraDon <GwaraDon@.github.com>
2019-10-17 12:45:42 +0545

    install packages

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import "bootstrap"
+import "bootstrap/scss/bootstrap.scss";
+import "../src/styles/style.scss";
+
+
 Vue.config.productionTip = false
 
 new Vue({
