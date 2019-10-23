dc32e452fe5ef6c23d1b267a922f15d16c0a1843
GwaraDon <GwaraDon@.github.com>
2019-10-23 16:56:46 +0545

    search filter bug fix

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,44 @@
 <template>
   <div id="app">
-    <div id="nav" class="d-flex justify-content-between">
-      <div class="bar" v-on:click="myFilter " :class="{ active: isActive }">
-        <span class="first"></span>
-        <span class="second"></span>
-        <span class="third"></span>
-      </div>
-      <div
-        :class="{ active: isActive }"
-        class="w-10"
-        style="
-      
-          height: 100vh;
-          background: rgb(246, 104, 46);
-          top: 0;
-          left:0;
-          position: absolute;
-          bottom: 0;
-          "
-      >
-        <transition name="fade">
-          <div class="show_hide--menu d-flex flex-column align-items-center" v-if="isActive">
-            <router-link to="/about">Home</router-link>
-            <router-link to="/about">Browse</router-link>
-            <router-link to="/about">Contact</router-link>
-            <router-link to="/about">About</router-link>
-          </div>
-        </transition>
-      </div>
-      <router-link to="/">
-        <img alt="Vue logo" src="./assets/justnep.svg" style=" width: 180px;" />
-      </router-link>
-
-      <div class="nav-link">
-
-        <button class="btn-orange px-4 py-2 mx-1" v-on:click="addActive ">Login</button>
-        <button class="btn-orange px-4 py-2" v-on:click="addActive ">Sign Up</button>
-        <!-- <router-link to="/about" class="btn-orange">Become a Rider</router-link> -->
-      </div>
+    <div v-if="$route.path == '/'">
+     <HomeNav v-bind:addActive="this.addActive"></HomeNav>
     </div>
-    <div v-if="!isHidden" :class="{ active: !isHidden }">
-      <Login />
+    <div :class="{ active: !isHidden }">
+      <div class="overlay" v-on:click="addActive"></div>
+      <transition name="slide-right-left" key="2">
+        <Login v-if="!isHidden" v-bind:addActive="this.addActive" />
+      </transition>
     </div>
     <router-view />
   </div>
 </template>
 <script>
 import Login from "@/components/Login.vue";
+import  HomeNav from "@/components/HomeNav.vue";
 export default {
-  components: {
-    Login
-  },
-
   data() {
     return {
-      isActive: false,
       isHidden: true
     };
   },
-
   methods: {
-    myFilter: function() {
-      this.isActive = !this.isActive;
-      // some code to filter users
-    },
     addActive() {
       this.isHidden = !this.isHidden;
+    },
+    debug() {
+      var curr = window.location.pathname; // eslint-disable-line
+      console.log(curr); // eslint-disable-line no-console
     }
+  },
+  components: {
+    Login,
+    HomeNav
   }
 };
 </script>
 
 <style lang="scss">
-.active .overlay {
+.overlay {
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.5);
@@ -82,7 +48,15 @@ export default {
   left: 0;
   right: 0;
   cursor: pointer;
+  opacity: 0;
+  visibility: hidden;
+  transition: all ease-in-out 0.5s;
 }
+.active .overlay {
+  visibility: visible;
+  opacity: 1;
+}
+
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s;
@@ -95,88 +69,25 @@ export default {
  {
   opacity: 0;
 }
+.slide-right-left-enter-active,
+.slide-right-left-leave-active {
+  transform: translateX(0) !important;
+  transition: transform 0.5s;
+  overflow: hidden;
+}
+.slide-right-left-enter,
+.slide-right-left-leave-to {
+  transform: translateX(100%) !important;
+  transition: transform 0.5s;
+  overflow: hidden;
+}
 
 #app {
   /*background: #000000;*/
   background: #ffffff;
   overflow: hidden;
 
-  .w-10 {
-    width: calc(100vw / 12 - 50px);
-    transition: width 0.5s;
-  }
 
-  .w-10.active {
-    width: calc(100vw / 12) !important;
-    transition: width 0.5s;
-  }
-
-  .bar {
-    position: absolute;
-    left: 0;
-    padding: 10px 12px;
-    z-index: 2;
-
-    span {
-      width: 30px;
-      height: 2px;
-      background: #fff;
-      display: block;
-      margin: 5px 0;
-      transition: all ease 0.4s;
-    }
-
-    span.second {
-      width: 40px !important;
-    }
-
-    &.active span.second {
-      width: 0 !important;
-    }
-
-    &.active span.first {
-      transform: rotate(45deg) translate(10px);
-    }
-
-    &.active span.third {
-      transform: rotate(-45deg) translate(10px);
-    }
-  }
-
-  .show_hide--menu {
-    position: absolute;
-    top: 100px;
-    left: 0;
-
-    a {
-      padding: 20px !important;
-    }
-  }
-
-  #nav {
-    padding: 11px 100px;
-    position: relative;
-    z-index: 1;
-
-    a {
-      color: #eee;
-      padding: 0px 20px;
-      margin-right: 10px;
-
-      .router-link-exact-active {
-        color: #fff;
-      }
-    }
-
-    .nav-link {
-      display: flex;
-      align-items: center;
-      padding: 0;
-
-      a {
-        padding: 10px 20px;
-      }
-    }
-  }
+ 
 }
 </style>
---
+++ src/components/HomeNav.vue
@@ -0,0 +1,79 @@
+<template>
+     <div id="nav" class="d-flex justify-content-between">
+      <SideNav></SideNav>
+      <router-link to="/">
+        <div class="logo">
+         <img alt="Vue logo" src="../assets/justnep.svg"  />
+        </div>
+      </router-link>
+      <div class="nav-link">
+        <button class="btn-orange px-4 py-2 mx-1" v-on:click="addActive">
+          <span class="d-sm-block d-none">Login</span>
+          <span class="d-sm-none d-block">
+            <i class="icofont-user-alt-7"></i>
+          </span>
+        </button>
+        <button class="btn-orange px-4 py-2 no-wrap">
+          <span class="d-sm-block d-none">Sign Up</span>
+          <span class="d-sm-none d-block">
+            <i class="icofont-fast-food"></i>
+          </span>
+        </button>
+        <!-- <router-link to="/about" class="btn-orange">Become a Rider</router-link> -->
+      </div>
+    </div>   
+    
+</template>
+<script>
+import SideNav from "@/components/SideNav.vue";
+export default {
+    name:'homeNav',
+     props: {
+      addActive:{
+          type:Function
+      }
+  },
+    data(){
+        return{
+
+        }
+    },
+    components:{
+        SideNav
+    }
+
+}
+</script>
+<style lang="scss">
+     #nav {
+    padding: 11px 100px;
+    position: relative;
+    z-index: 1;
+    .logo {
+      max-width: 180px;
+      min-width: 100px;
+      img{
+        width: 100%;
+      }
+    }
+    a {
+      color: #eee;
+      padding: 0px 20px;
+      margin-right: 10px;
+
+      .router-link-exact-active {
+        color: #fff;
+      }
+    }
+
+    .nav-link {
+      display: flex;
+      align-items: center;
+      padding: 0;
+
+      a {
+        padding: 10px 20px;
+      }
+    }
+  }
+</style>
