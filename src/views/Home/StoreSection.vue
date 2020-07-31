1a773cab9398d5a99da956259c08d5bba05916dd
stharohit <stha.rht028@gmail.com>
2020-07-31 11:27:45 +0545

    replace commit

---
+++ src/views/Home/StoreSection.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container py-5">
+    <div class="row">
+      <div class="col-md-6">
+        <h1 class="mb-3">
+          Restaurants in your <br />
+          pocket
+        </h1>
+        <p class="mb-5">
+          <em
+            >"Get our app, it's the fastest way to order food on the go. It is
+            Fast when You want to Chop. Winks!"</em
+          >
+        </p>
+        <ul class="d-flex">
+          <li class="mr-4">
+            <a
+              href="https://apps.apple.com/app/id1513066186"
+              target="_blank"
+              class="btn-store btn-appstore"
+              ><img src="@/assets/appstore.png" alt="App Store"
+            /></a>
+          </li>
+          <li>
+            <a
+              href="https://play.google.com/store/apps/details?id=com.justnep.customer"
+              target="_blank"
+              class="btn-store btn-googleplay"
+              ><img src="@/assets/playstore.png" alt="Play Store"
+            /></a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-6">
+        <img src="@/assets/justnep-app.png" alt="Justnep App features" />
+      </div>
+    </div>
+  </div>
+</template>
